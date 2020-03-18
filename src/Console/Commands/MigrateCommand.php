6e204f52091dd0c6980908dbd54e661422a085f6
Abdelrahman Omran <me@omranic.com>
2020-03-18 15:16:21 +0400

    Fix migrations path condition

--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -31,7 +31,11 @@ class MigrateCommand extends Command
     {
         $this->alert($this->description);
 
-        if (file_exists($path = 'database/migrations/rinvex/laravel-statistics')) {
+        $path = config('rinvex.statistics.autoload_migrations') ?
+            'vendor/rinvex/laravel-statistics/database/migrations' :
+            'database/migrations/rinvex/laravel-statistics';
+
+        if (file_exists($path)) {
             $this->call('migrate', [
                 '--step' => true,
                 '--path' => $path,
