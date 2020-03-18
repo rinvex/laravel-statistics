9ff2c14d0295233760c4ea0e68d925abaa09ea3e
Abdelrahman Omran <me@omranic.com>
2020-03-18 15:05:32 +0400

    Add shortcut -f (force) for artisan publish commands

--- src/Console/Commands/RollbackCommand.php
+++ src/Console/Commands/RollbackCommand.php
@@ -13,7 +13,7 @@ class RollbackCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'rinvex:rollback:statistics {--force : Force the operation to run when in production.}';
+    protected $signature = 'rinvex:rollback:statistics {--f|force : Force the operation to run when in production.}';
 
     /**
      * The console command description.
